1a779cf609d57d09cafceaf3caae2110fcc9e1d3
Lion <20693521+lion-zhang@users.noreply.github.com>
2022-06-27 13:22:37 -0400

    test case

---
+++ rtss/4_recovery_given_k_kalman.py
@@ -0,0 +1,211 @@
+from time import perf_counter
+from copy import deepcopy
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+from settings_kf import motor_speed_bias
+from utils.formal.gaussian_distribution import GaussianDistribution
+from utils.formal.reachability import ReachableSet
+from utils.formal.zonotope import Zonotope
+from utils.observers.kalman_filter import KalmanFilter
+
+exps = [motor_speed_bias]
+result = {}   # for print or plot
+for exp in exps:
+    # ----------------------------------- w/o kalman filter ---------------------------
+    print('=' * 20, exp.name, ' w/o kalman filter', '=' * 20)
+    k = None
+    rec_u = None
+    recovery_complete_index = None
+
+    u_lo = exp.model.controller.control_lo
+    u_up = exp.model.controller.control_up
+    U = Zonotope.from_box(u_lo, u_up)
+    A = exp.model.sysd.A
+    B = exp.model.sysd.B
+    W = exp.model.p_noise_dist
+    reach = ReachableSet(A, B, U, W, max_step=100)
+
+    A = exp.model.sysd.A
+    B = exp.model.sysd.B
+    C = exp.kf_C
+    D = exp.model.sysd.D
+    Q = exp.model.p_noise_dist.sigma if exp.model.p_noise_dist is not None else np.zeros_like(A)
+    R = np.zeros((C.shape[0], C.shape[0]))
+    kf = KalmanFilter(A, B, C, D, Q, R)
+    for i in range(0, exp.max_index + 1):
+        assert exp.model.cur_index == i
+        exp.model.update_current_ref(exp.ref[i])
+        # attack here
+        exp.model.cur_feedback = exp.attack.launch(exp.model.cur_feedback, i, exp.model.states)
+        if i == exp.attack_start_index - 1:
+            print('normal_state=', exp.model.cur_x)
+        if i == exp.recovery_index:
+            print('recovery_start_state=', exp.recovery_index)
+            print('recovery_start_state=', exp.model.cur_x)
+            us = exp.model.inputs[exp.attack_start_index:exp.recovery_index]
+            ys = (C @ exp.model.states[exp.attack_start_index+1:exp.recovery_index+1].T).T
+            x_0 = exp.model.states[exp.attack_start_index]
+            x_0 = GaussianDistribution(x_0, np.zeros((exp.model.n, exp.model.n)))
+            reach.init(x_0, exp.s)
+            x_res_point = reach.state_reconstruction(us)
+            print('x_0=', x_res_point)
+
+            reach.init(x_res_point, exp.s)
+            # k, satisfy, X_k, D_k, z_star, alpha, P, arrive = reach.given_P(P_given=exp.P_given, max_k=40)
+            # k, satisfy, X_k, D_k, z_star, alpha, P, arrive = reach.given_P(P_given=given_P, max_k=50)
+            k, X_k, D_k, z_star, alpha, P, arrive = reach.given_k(max_k=50)
+            recovery_complete_index = exp.recovery_index + k
+            print('k=', k, 'P=', P, 'z_star=', z_star, 'arrive=', arrive)
+            print('D_k=', D_k)
+            print('recovery_complete_index=', recovery_complete_index)
+            rec_u = U.alpha_to_control(alpha)
+            print(rec_u)
+        if exp.recovery_index <= i < recovery_complete_index:
+            print(i)
+            print(exp.model.cur_x)
+            rec_u_index = i - exp.recovery_index
+            u = rec_u[rec_u_index]
+            exp.model.evolve(u)
+        else:
+            if i == recovery_complete_index:
+                print('state after recovery:', exp.model.cur_x)
+            exp.model.evolve()
+
+    result['w/'] = {}
+    result['w/']['outputs'] = deepcopy(exp.model.outputs)
+    result['w/']['recovery_complete_index'] = recovery_complete_index
+
+    # ----------------------------------- w/ kalman filter ---------------------------
+    exp.model.reset()
+    print('=' * 20, exp.name, '=' * 20)
+    k = None
+    rec_u = np.zeros((100, exp.model.m))
+    recovery_complete_index = None
+    x_cur_predict = None
+    x_cur_update = None
+
+    u_lo = exp.model.controller.control_lo
+    u_up = exp.model.controller.control_up
+    U = Zonotope.from_box(u_lo, u_up)
+    A = exp.model.sysd.A
+    B = exp.model.sysd.B
+    W = exp.model.p_noise_dist
+    reach = ReachableSet(A, B, U, W, max_step=100)
+
+    A = exp.model.sysd.A
+    B = exp.model.sysd.B
+    C = exp.kf_C
+    D = exp.model.sysd.D
+    Q = exp.model.p_noise_dist.sigma if exp.model.p_noise_dist is not None else np.zeros_like(A)
+    R = np.zeros((C.shape[0], C.shape[0]))
+    kf = KalmanFilter(A, B, C, D, Q, R)
+    for i in range(0, exp.max_index + 1):
+        assert exp.model.cur_index == i
+        exp.model.update_current_ref(exp.ref[i])
+        # attack here
+        exp.model.cur_feedback = exp.attack.launch(exp.model.cur_feedback, i, exp.model.states)
+        if i == exp.attack_start_index - 1:
+            print('normal_state=', exp.model.cur_x)
+        if i == exp.recovery_index:
+            print('recovery_start_state=', exp.recovery_index)
+            print('recovery_start_state=', exp.model.cur_x)
+            print("-" * 15, i, "-" * 15)
+            us = exp.model.inputs[exp.attack_start_index:exp.recovery_index]
+            ys = (C @ exp.model.states[exp.attack_start_index + 1:exp.recovery_index + 1].T).T
+            x_0 = exp.model.states[exp.attack_start_index]
+            x_res, P_res = kf.multi_steps(x_0, np.zeros_like(A), us, ys)
+            x_cur_update = GaussianDistribution(x_res[-1], P_res[-1])
+            print('x_cur_update=', x_cur_update)
+            print('x_cur_real=', exp.model.cur_x)
+
+            reach.init(x_cur_update, exp.s)
+            k, X_k, D_k, z_star, alpha, P, arrive = reach.given_k(max_k=50)
+            recovery_complete_index = exp.recovery_index + k
+            rec_u_temp = U.alpha_to_control(alpha)
+            rec_u_index = i - exp.recovery_index
+            rec_u[rec_u_index] = rec_u_temp[0]
+            # print(rec_u)
+        if exp.recovery_index < i < recovery_complete_index:
+            print("-" * 15, i, "-" * 15)
+            x_cur_predict = GaussianDistribution(*kf.predict(x_cur_update.miu, x_cur_update.sigma, exp.model.cur_u))
+            y = C @ exp.model.cur_x
+            x_cur_update = GaussianDistribution(*kf.update(x_cur_predict.miu, x_cur_predict.sigma, y))
+            print('x_cur_update=', x_cur_update)
+            print('x_cur_real=', exp.model.cur_x)
+
+            reach.init(x_cur_update, exp.s)
+            k, X_k, D_k, z_star, alpha, P, arrive = reach.given_k(max_k=50)
+            print('k=', k, ' D_k=', D_k)
+            rec_u_temp = U.alpha_to_control(alpha)
+            rec_u_index = i - exp.recovery_index
+            rec_u[rec_u_index] = rec_u_temp[0]
+            print('u=', rec_u_temp[0])
+
+        if exp.recovery_index <= i < recovery_complete_index:
+            rec_u_index = i - exp.recovery_index
+            u = rec_u[rec_u_index]
+            exp.model.evolve(u)
+            print(exp.model.cur_x)
+        else:
+            if i == recovery_complete_index:
+                print('state after recovery:', exp.model.cur_x)
+            exp.model.evolve()
+
+    result['w/o'] = {}
+    result['w/o']['outputs'] = deepcopy(exp.model.outputs)
+    result['w/o']['recovery_complete_index'] = recovery_complete_index
+
+    # # plot
+    # t_arr = np.linspace(0, exp.dt * exp.max_index, exp.max_index + 1)
+    # # output
+    # for i in range(exp.model.p):
+    #     ref = [x[i] for x in exp.model.refs[:exp.max_index + 1]]
+    #     y_arr = [x[i] for x in exp.model.outputs[:exp.max_index + 1]]
+    #     fig = plt.figure()
+    #     plt.title(exp.name + ' y_' + str(i))
+    #     plt.plot(t_arr, ref, color='black', linestyle='dashed')
+    #     plt.plot(t_arr, y_arr)
+    #     # t_rec_arr = np.linspace(exp.dt * exp.attack_start_index, exp.dt * exp.recovery_index, (exp.recovery_index-exp.attack_start_index)+1)
+    #     # y_rec_arr = (exp.model.sysd.C @ x_res.T).T
+    #     # plt.plot(t_rec_arr, y_rec_arr)
+    #     plt.show()
+    # # control input
+    # for i in range(exp.model.m):
+    #     u_arr = [x[i] for x in exp.model.inputs[:exp.max_index + 1]]
+    #     fig = plt.figure()
+    #     plt.title(exp.name + ' u_' + str(i))
+    #     plt.plot(t_arr, u_arr)
+    #     plt.show()
+
+    P_final = D_k.prob_in_strip(exp.s)
+    print(f'P_final={P_final}')
+
+    # plot
+    plt.rcParams.update({'font.size': 18})  # front sizw
+    fig = plt.figure(figsize=(8, 4))
+    plt.title(exp.name+' y_'+str(exp.output_index))
+    recovery_complete_index = result['w/o']['recovery_complete_index']
+    t_arr = np.linspace(0, exp.dt * recovery_complete_index, recovery_complete_index + 1)
+    y_arr = [x[exp.output_index] for x in result['w/o']['outputs'][:recovery_complete_index + 1]]
+    ref = [x[exp.ref_index] for x in exp.model.refs[:recovery_complete_index + 1]]
+    plt.plot(t_arr, ref, color='black', linestyle='dashed')
+    plt.plot(t_arr, y_arr, label='w/o')
+
+    recovery_complete_index = result['w/']['recovery_complete_index']
+    t_arr = np.linspace(0, exp.dt * recovery_complete_index, recovery_complete_index + 1)
+    y_arr = [x[exp.output_index] for x in result['w/']['outputs'][:recovery_complete_index + 1]]
+    plt.plot(t_arr, y_arr, label='w/')
+
+    plt.vlines(exp.attack_start_index*exp.dt, exp.y_lim[0], exp.y_lim[1], colors='red', linestyle='dashed')
+    plt.vlines(exp.recovery_index*exp.dt, exp.y_lim[0], exp.y_lim[1], colors='green', linestyle='dotted', linewidth=2.5)
+    plt.hlines(exp.strip[0], exp.x_lim, recovery_complete_index*exp.dt, colors='grey')
+    plt.hlines(exp.strip[1], exp.x_lim, recovery_complete_index * exp.dt, colors='grey')
+    plt.ylim(exp.y_lim)
+    plt.xlim(exp.x_lim, recovery_complete_index*exp.dt)
+    plt.legend(loc='best')
+    # plt.savefig('./fig/'+exp.name+'.pdf', format='pdf', bbox_inches='tight')
+    plt.show()
+    # output
+
---
+++ rtss/settings_kf.py
@@ -0,0 +1,320 @@
+import numpy as np
+from simulators.linear.motor_speed import MotorSpeed
+from simulators.linear.quadruple_tank import QuadrupleTank
+from simulators.linear.F16 import F16
+from simulators.linear.aircraft_pitch import AircraftPitch
+from simulators.linear.boeing747 import Boeing
+from simulators.linear.heat import Heat
+from simulators.linear.platoon import Platoon
+from simulators.linear.rlc_circuit import RlcCircuit
+from simulators.linear.quadrotor import Quadrotor
+from utils.attack import Attack
+from utils.formal.strip import Strip
+
+
+# --------------------- motor speed -------------------
+class motor_speed_bias:
+    # needed by 0_attack_no_recovery
+    name = 'motor_speed_bias'
+    max_index = 500
+    dt = 0.02
+    ref = [np.array([4])] * 501
+    noise = {
+        'process': {
+            'type': 'white',
+            'param': {'C': np.array([[0.1, 0], [0, 0.4]])}
+        }
+    }
+    model = MotorSpeed('bias', dt, max_index, noise)
+    control_lo = np.array([0])
+    control_up = np.array([60])
+    model.controller.set_control_limit(control_lo, control_up)
+    attack_start_index = 150
+    bias = np.array([-1])
+    attack = Attack('bias', bias, attack_start_index)
+    recovery_index = 200
+
+    # needed by 1_recovery_given_p
+    s = Strip(np.array([-1, 0]), a=-4.3, b=-3.7)
+    P_given = 0.95
+    max_recovery_step = 40
+    # plot
+    ref_index = 0
+    output_index = 0
+    x_lim = 2.8
+    y_lim = (3.65, 5.5)
+    y_label = 'rotational speed - rad/sec'
+    strip = (4.3, 3.7)
+
+    kf_C = np.array([[0, 1]])
+
+
+# -------------------- quadruple tank ----------------------------
+class quadruple_tank_bias:
+    # needed by 0_attack_no_recovery
+    name = 'quadruple_tank_bias'
+    max_index = 200
+    dt = 1
+    ref = [np.array([7, 7])] * 1001 + [np.array([7, 7])] * 1000
+    noise = {
+        'process': {
+            'type': 'white',
+            'param': {'C': np.eye(4) * 0.1}
+        }
+    }
+    model = QuadrupleTank('test', dt, max_index, noise)
+    control_lo = np.array([0, 0])
+    control_up = np.array([10, 10])
+    model.controller.set_control_limit(control_lo, control_up)
+    attack_start_index = 80
+    bias = np.array([-2.0, 0])
+    attack = Attack('bias', bias, attack_start_index)
+    recovery_index = 120
+
+    # needed by 1_recovery_given_p
+    s = Strip(np.array([-1, 0, 0, 0]), a=-15, b=-14.6)
+    P_given = 0.95
+    max_recovery_step = 40
+    # plot
+    ref_index = 0
+    output_index = 0
+    x_lim = 75
+    y_lim = (6, 10)
+    y_label = 'water level - cm'
+    strip = (7.3, 6.7)
+
+
+# -------------------- f16 ----------------------------
+class f16_bias:
+    # needed by 0_attack_no_recovery
+    name = 'f16_bias'
+    max_index = 800
+    dt = 0.02
+    ref = [np.array([0.0872665 * 57.3])] * 801
+    noise = {
+        'process': {
+            'type': 'white',
+            'param': {'C': np.eye(4) * 0.00001}
+        }
+    }
+    model = F16('test', dt, max_index, noise)
+    control_lo = np.array([-25])
+    control_up = np.array([25])
+    model.controller.set_control_limit(control_lo, control_up)
+    attack_start_index = 400
+    bias = np.array([-1])
+    attack = Attack('bias', bias, attack_start_index)
+    recovery_index = 600
+
+    # needed by 1_recovery_given_p
+    s = Strip(np.array([0, 0, -1, 0]), a=-8.76e-02, b=8.75e-02)
+    P_given = 0.95
+    max_recovery_step = 40
+    # plot
+    ref_index = 0
+    output_index = 0
+    x_lim = 7.5
+    y_lim = (4.5, 7)
+    y_label = 'pitch angle - rad'
+    strip = (8.76e-02*57.3, 8.75e-02*57.3)
+
+# -------------------- aircraft_pitch ----------------------------
+class aircraft_pitch_bias:
+    # needed by 0_attack_no_recovery
+    name = 'aircraft_pitch_bias'
+    max_index = 1500
+    dt = 0.02
+    ref = [np.array([0.2])] * (max_index + 1)
+    noise = {
+        'process': {
+            'type': 'white',
+            'param': {'C': np.eye(3) * 0.00001}
+        }
+    }
+    model = AircraftPitch('aircraft_pitch', dt, max_index, noise)
+    control_lo = np.array([-20])
+    control_up = np.array([20])
+    model.controller.set_control_limit(control_lo, control_up)
+    attack_start_index = 500
+    bias = np.array([-1])
+    attack = Attack('bias', bias, attack_start_index)
+    recovery_index = 700
+
+    # needed by 1_recovery_given_p
+    s = Strip(np.array([0, 0, -1]), a=-0.33, b=--0.27)
+    P_given = 0.95
+    max_recovery_step = 40
+    # plot
+    ref_index = 0
+    output_index = 0
+    x_lim = 8
+    y_lim = (0, 1.2)
+    y_label = 'pitch - rad'
+    strip = (0.33, 0.27)
+
+
+# -------------------- boeing747 ----------------------------
+class boeing747_bias:
+    # needed by 0_attack_no_recovery
+    name = 'boeing747'
+    max_index = 800
+    dt = 0.02
+    ref = [np.array([1])] * (max_index + 1)
+    noise = {
+        'process': {
+            'type': 'white',
+            'param': {'C': np.eye(5) * 0.0001}
+        }
+    }
+    model = Boeing('boeing747', dt, max_index, noise)
+    control_lo = np.array([-30])
+    control_up = np.array([30])
+    model.controller.set_control_limit(control_lo, control_up)
+    attack_start_index = 400
+    bias = np.array([-1])
+    attack = Attack('bias', bias, attack_start_index)
+    recovery_index = 500
+
+    # needed by 1_recovery_given_p
+    s = Strip(np.array([-1, 0, 0, 0, 0]), a=-1.3, b=-0.7)
+    P_given = 0.95
+    max_recovery_step = 40
+    # plot
+    ref_index = 0
+    output_index = 0
+    x_lim = 7.5
+    y_lim = (0.4, 2.2)
+    y_label = 'Yaw angle - rad'
+    strip = (0.7, 1.3)
+
+# -------------------- heat ----------------------------
+class heat_bias:
+    # needed by 0_attack_no_recovery
+    name = 'heat'
+    max_index = 1000
+    dt = 1
+    ref = [np.array([15])] * (max_index + 1)
+    noise = {
+        'process': {
+            'type': 'white',
+            'param': {'C': np.eye(45) * 0.0001}
+        }
+    }
+    model = Heat('heat', dt, max_index, noise)
+    control_lo = np.array([-0.5])
+    control_up = np.array([50])
+    model.controller.set_control_limit(control_lo, control_up)
+    attack_start_index = 400
+    bias = np.array([-1])
+    attack = Attack('bias', bias, attack_start_index)
+    recovery_index = 500
+
+    # needed by 1_recovery_given_p
+    l = np.zeros((45,))
+    y_point = (45 + 1) // 3 * 2 - 1
+    l[y_point] = -1
+    s = Strip(l, a=-15.3, b=-14.7)
+    P_given = 0.95
+    max_recovery_step = 60
+    # plot
+    ref_index = 0
+    output_index = 0
+
+# -------------------- platoon ----------------------------
+class platoon_bias:
+    # needed by 0_attack_no_recovery
+    name = 'platoon'
+    max_index = 800
+    dt = 0.02
+    ref = [np.array([2])] * (max_index + 1)
+    noise = {
+        'process': {
+            'type': 'white',
+            'param': {'C': np.eye(7) * 0.0001}
+        }
+    }
+    model = Platoon('Platoon', dt, max_index, noise)
+    control_lo = np.array([-5])
+    control_up = np.array([5])
+    model.controller.set_control_limit(control_lo, control_up)
+    attack_start_index = 400
+    bias = np.array([-1])
+    attack = Attack('bias', bias, attack_start_index)
+    recovery_index = 500
+
+    # needed by 1_recovery_given_p
+    s = Strip(np.array([-1, 0, 0, 0, 0, 0, 0]), a=-2.3, b=-1.7)
+    P_given = 0.95
+    max_recovery_step = 40
+    # plot
+    ref_index = 0
+    output_index = 0
+
+
+# -------------------- rlc_circuit ----------------------------
+class rlc_circuit_bias:
+    # needed by 0_attack_no_recovery
+    name = 'rlc_circuit'
+    max_index = 600
+    dt = 0.02
+    ref = [np.array([3])] * (max_index + 1)
+    noise = {
+        'process': {
+            'type': 'white',
+            'param': {'C': np.eye(2) * 0.0001}
+        }
+    }
+    model = RlcCircuit('rlc_circuit', dt, max_index, noise)
+    control_lo = np.array([-15])
+    control_up = np.array([15])
+    model.controller.set_control_limit(control_lo, control_up)
+    attack_start_index = 400
+    bias = np.array([-1])
+    attack = Attack('bias', bias, attack_start_index)
+    recovery_index = 500
+
+    # needed by 1_recovery_given_p
+    s = Strip(np.array([-1, 0]), a=-3.3, b=-2.7)
+    P_given = 0.95
+    max_recovery_step = 40
+    # plot
+    ref_index = 0
+    output_index = 0
+    x_lim = 7.7
+    y_lim = (2.6, 4.1)
+    y_label = 'Capacitor Voltage - V'
+    strip = (2.7, 3.3)
+
+# -------------------- quadrotor ----------------------------
+class quadrotor_bias:
+    # needed by 0_attack_no_recovery
+    name = 'quadrotor'
+    max_index = 600
+    dt = 0.02
+    ref = [np.array([4])] * (max_index + 1)
+    noise = {
+        'process': {
+            'type': 'white',
+            'param': {'C': np.eye(12) * 0.0001}
+        }
+    }
+    model = Quadrotor('quadrotor', dt, max_index, noise)
+    control_lo = np.array([-50])
+    control_up = np.array([50])
+    model.controller.set_control_limit(control_lo, control_up)
+    attack_start_index = 400
+    bias = np.array([-1])
+    attack = Attack('bias', bias, attack_start_index)
+    recovery_index = 500
+
+    # needed by 1_recovery_given_p
+    s = Strip(np.array([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, -1]), a=-4.3, b=-3.7)
+    P_given = 0.95
+    max_recovery_step = 40
+    # plot
+    ref_index = 0
+    output_index = 5
+    x_lim = 7.7
+    y_lim = (3.6, 5.2)
+    y_label = 'Altitude - m'
+    strip = (4.3, 3.7)
