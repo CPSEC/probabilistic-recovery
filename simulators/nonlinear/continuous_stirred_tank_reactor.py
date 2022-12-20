9bfce39217dc7f347c259f84e2bc05bb1d21db48
Lion <20693521+lion-zhang@users.noreply.github.com>
2022-12-20 11:22:07 -0500

    add non-linear benchmarks

---
+++ simulators/nonlinear/continuous_stirred_tank_reactor.py
@@ -0,0 +1,168 @@
+import numpy as np
+from utils import Simulator
+from utils.controllers.PID_incremental import PID
+from interval import imath
+
+# Parameters:
+# Volumetric Flowrate (m^3/sec)
+q = 100
+# Volume of CSTR (m^3)
+V = 100
+# Density of A-B Mixture (kg/m^3)
+rho = 1000
+# Heat capacity of A-B Mixture (J/kg-K)
+Cp = 0.239
+# Heat of reaction for A->B (J/mol)
+mdelH = 5e4
+# E - Activation energy in the Arrhenius Equation (J/mol)
+# R - Universal Gas Constant = 8.31451 J/mol-K
+EoverR = 8750
+# Pre-exponential factor (1/sec)
+k0 = 7.2e10
+# U - Overall Heat Transfer Coefficient (W/m^2-K)
+# A - Area - this value is specific for the U calculation (m^2)
+UA = 5e4
+
+
+def cstr(t, x, u, Tf=350, Caf=1, use_imath=False):
+    # Inputs (3):
+    # Temperature of cooling jacket (K)
+    Tc = u[0]
+    # Tf = Feed Temperature (K)
+    # Caf = Feed Concentration (mol/m^3)
+
+    # States (2):
+    # Concentration of A in CSTR (mol/m^3)
+    Ca = x[0]
+    # Temperature in CSTR (K)
+    T = x[1]
+
+    # reaction rate
+    if use_imath:
+        rA = k0 * imath.exp(-EoverR / T) * Ca
+    else:
+        rA = k0 * np.exp(-EoverR / T) * Ca
+
+    # Calculate concentration derivative
+    dCadt = q / V * (Caf - Ca) - rA
+    # Calculate temperature derivative
+    dTdt = q / V * (Tf - T) \
+           + mdelH / (rho * Cp) * rA \
+           + UA / V / rho / Cp * (Tc - T)
+
+    # Return xdot:
+    if use_imath:
+        return [dCadt, dTdt]
+    else:
+        xdot = np.zeros(2)
+        xdot[0] = dCadt
+        xdot[1] = dTdt
+        return xdot
+
+def cstr_imath(t, x, u, Tf=350, Caf=1):
+    return cstr(t=t, x=x, u=u, Tf=Tf, Caf=Caf, use_imath=True)
+
+# initial states
+x_0 = np.array([0.98, 280])
+
+# control parameters
+KP = 0.5 * 1.0
+KI =  KP / (3 / 8.0)
+KD =  - KP * 0.1
+u_0 = 274.57786
+control_limit = {
+    'lo': np.array([250]),
+    'up': np.array([350])
+}
+
+
+class Controller:
+    def __init__(self, dt):
+        self.dt = dt
+        self.pid = PID(u_0, KP, KI, KD, current_time=-dt)
+        self.pid.setWindup(100)
+        self.pid.setSampleTime(dt)
+        self.set_control_limit(control_limit['lo'], control_limit['up'])
+
+    def update(self, ref: np.ndarray, feedback_value: np.ndarray, current_time) -> np.ndarray:
+        self.pid.set_reference(ref[1])             # only care about the 2nd state here
+        cin = self.pid.update(feedback_value[1], current_time)  # only use the 2nd state here
+        return np.array([cin])
+
+    def set_control_limit(self, control_lo, control_up):
+        self.control_lo = control_lo
+        self.control_up = control_up
+        self.pid.set_control_limit(self.control_lo[0], self.control_up[0])
+
+    def clear(self):
+        self.pid.clear(current_time=-self.dt)
+
+
+class CSTR(Simulator):
+    """
+        States: (2,)
+            x[0]: Concentration of A in CSTR (mol/m^3)
+            x[1]: Temperature in CSTR (K)
+        Control Input: (1,)
+            u[0]: Temperature of cooling jacket (K)
+        Output:  (2,)
+            State Feedback
+        Controller: PID
+    """
+
+    def __init__(self, name, dt, max_index, noise=None):
+        super().__init__('CSTR ' + name, dt, max_index)
+        self.nonlinear(ode=cstr, n=2, m=1, p=2)  # number of states, control inputs, outputs
+        controller = Controller(dt)
+        settings = {
+            'init_state': x_0,
+            'feedback_type': 'state',  # 'state' or 'output',  you must define C if 'output'
+            'controller': controller
+        }
+        if noise:
+            settings['noise'] = noise
+        self.sim_init(settings)
+
+
+if __name__ == "__main__":
+    max_index = 1000
+    dt = 0.02
+    ref = [np.array([0, 300])] * (max_index+1)
+    # noise = {
+    #     'process': {
+    #         'type': 'box_uniform',
+    #         'param': {'lo': np.array([-0.000001, -0.001]), 'up': np.array([0.001, 0.001])}
+    #     }
+    # }
+    noise = None
+    cstr_model = CSTR('test', dt, max_index, noise)
+
+
+    for i in range(0, max_index + 1):
+        assert cstr_model.cur_index == i
+        cstr_model.update_current_ref(ref[i])
+        # attack here
+
+        # x = A x + B u
+        # x, u - 1-D array
+        # cstr_model.cur_x   #  ground truth
+        # cstr_model.cur_u   #  control input of last step
+        # cstr_model.cur_y   #
+        # cstr_model.cur_feedback   # attack on this
+
+        cstr_model.evolve()
+
+
+    # print results
+    import matplotlib.pyplot as plt
+
+    t_arr = np.linspace(0, 10, max_index + 1)
+    ref = [x[1] for x in cstr_model.refs[:max_index + 1]]
+    y_arr = [x[1] for x in cstr_model.outputs[:max_index + 1]]
+
+    plt.plot(t_arr, y_arr, t_arr, ref)
+    plt.show()
+
+    u_arr = [x[0] for x in cstr_model.inputs[:max_index + 1]]
+    plt.plot(t_arr, u_arr)
+    plt.show()
---
+++ simulators/nonlinear/quad.py
@@ -0,0 +1,171 @@
+# Ref: Data-Driven Science and Engineering: Machine Learning, Dynamical Systems, and Control (Session 8.7, Page 300)
+
+import numpy as np
+from utils import Simulator, PID
+import math 
+# parameters
+from interval import imath
+
+
+def quad(t, x, u, use_imath=False):
+    # Control Inputs (4):
+    # Only U_t is non-zero
+
+    # More Parameters:
+    Ix = 5 * 0.001
+    Iy = 5 * 0.001
+    Iz = 5 * 0.001 # values from https://repository.upenn.edu/cgi/viewcontent.cgi?article=1705&context=edissertations
+    mass = 1
+    g = 9.81
+
+    # States (12):
+    phi, theta, psi, w_phi, w_theta, w_psi, x, y, z, v_x, v_y, v_z = x
+    U_t = u[0]
+    U_phi = 0
+    U_theta = 0
+    U_psi  = 0
+    if use_imath: 
+        xdot = [
+        w_phi,
+        w_theta,
+        w_psi,
+        U_phi/Ix + w_theta * w_psi * (Iy - Iz)/Ix, # 0
+        U_theta/Iy + w_phi * w_psi * (Iz - Iz)/Iy, # 0
+        U_psi/Iz + w_phi * w_theta * (Ix - Iy)/Iz, # 0
+        v_x,
+        v_y,
+        v_z,
+        # Replaced theta with phi on RHS terms below based on https://arxiv.org/pdf/1602.02622.pdf
+        U_t/mass * (imath.cos(phi)* imath.sin(theta) * imath.cos(psi) + imath.sin(phi) * imath.sin(psi)),
+        U_t/mass * (imath.cos(phi)* imath.sin(theta) * imath.sin(psi) - imath.sin(phi) * imath.cos(psi)),
+        U_t/mass * imath.cos(phi) * imath.cos(theta) - g
+    ]
+    else:
+        xdot = np.array([
+        w_phi,
+        w_theta,
+        w_psi,
+        U_phi/Ix + w_theta * w_psi * (Iy - Iz)/Ix, # 0
+        U_theta/Iy + w_phi * w_psi * (Iz - Iz)/Iy, # 0
+        U_psi/Iz + w_phi * w_theta * (Ix - Iy)/Iz, # 0
+        v_x,
+        v_y,
+        v_z,
+        # Replaced theta with phi on RHS terms below based on https://arxiv.org/pdf/1602.02622.pdf
+        U_t/mass * (math.cos(phi)* math.sin(theta) * math.cos(psi) + math.sin(phi) * math.sin(psi)),
+        U_t/mass * (math.cos(phi)* math.sin(theta) * math.sin(psi) - math.sin(phi) * math.cos(psi)),
+        U_t/mass * math.cos(phi) * math.cos(theta) - g
+    ])
+    # xdot = [
+    #     0,
+    #     0,
+    #     0,
+    #     0, # 0
+    #     0, # 0
+    #     0, # 0
+    #     0,
+    #     0,
+    #     v_z,
+    #     # Replaced theta with phi on RHS terms below based on https://arxiv.org/pdf/1602.02622.pdf
+    #     0,
+    #     0,
+    #     U_t/mass - g
+    # ]
+    # Return xdot:
+    return xdot
+    
+def quad_imath(t, x, u):
+    return quad(t=t, x=x, u=u, use_imath=True)
+
+x_0 = np.array([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
+control_limit = {
+    'lo': np.array([-10]),
+    'up': np.array([100])
+}
+
+# control parameters
+
+
+KP =  100
+KI = 0
+KD = -19
+class Controller:
+    def __init__(self, dt):
+        self.dt = dt
+        self.pid = PID(KP, KI, KD, current_time=-dt)
+        self.pid.setWindup(0)
+        self.pid.setSampleTime(dt)
+        self.set_control_limit(control_limit['lo'], control_limit['up'])
+
+    def update(self, ref: np.ndarray, feedback_value: np.ndarray, current_time) -> np.ndarray:
+        self.pid.set_reference(ref[-4])
+        cin = self.pid.update(feedback_value[-4], current_time)
+        return np.array([cin])
+
+    def set_control_limit(self, control_lo, control_up):
+        self.control_lo = control_lo
+        self.control_up = control_up
+        self.pid.set_control_limit(self.control_lo[0], self.control_up[0])
+
+    def clear(self):
+        self.pid.clear(current_time=-self.dt)
+
+
+
+class quadrotor(Simulator):
+    """
+    States: (4,)
+        x[0]: location of cart
+        x[1]: dx[0]
+        x[2]: pendulum angle  (down:0, up:pi)
+        x[3]: dx[1]
+    Control Input: (1,)  [control_limit]
+        u[0]: force on the cart
+    Output: (4,)
+        State Feedback
+    Controller: LQR
+    """
+
+    def __init__(self, name, dt, max_index, noise=None):
+        super().__init__('Quadrotor ' + name, dt, max_index)
+        self.nonlinear(ode=quad, n=12, m=1, p=12)
+        controller = Controller(dt)
+        settings = {
+            'init_state': x_0,
+            'feedback_type': 'state',
+            'controller': controller
+        }
+        if noise:
+            settings['noise'] = noise
+        self.sim_init(settings)
+
+
+if __name__ == "__main__":
+    max_index = 300
+    dt = 0.01
+    ref = [np.array([0, 0, 0, 0, 0, 0, 0, 0, 10, 0, 0, 0])] * (max_index+1)
+    # bias attack example
+    from utils import Attack
+    bias = np.array([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
+    bias_attack = Attack('bias', bias, 300)
+    ip = quadrotor('test', dt, max_index)
+    for i in range(0, max_index + 1):
+        assert ip.cur_index == i
+        ip.update_current_ref(ref[i])
+        # attack here
+        # ip.cur_feedback = bias_attack.launch(ip.cur_feedback, ip.cur_index, ip.states)
+        ip.evolve()
+    # print results
+    import matplotlib.pyplot as plt
+
+    fig, ax = plt.subplots(2, 1)
+    ax1, ax2 = ax
+    t_arr = np.linspace(0, 3, max_index + 1)
+    ref2 = [x[-4] for x in ip.refs[:max_index + 1]]
+    y2_arr = [x[-4] for x in ip.outputs[:max_index + 1]]
+    ax1.set_title('Altitude')
+    ax1.plot(t_arr, y2_arr, t_arr, ref2)
+    u_arr = [x[0] for x in ip.inputs[:max_index + 1]]
+    ax2.set_title('u-force')
+    ax2.plot(t_arr, u_arr)
+    plt.show()
